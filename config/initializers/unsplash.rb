1dda7e3602d84278dbc29fbeb01d77993e122c80
Mike <themikejahn@gmail.com>
2020-03-15 21:10:39 -0500

    use ENV variables

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,6 +1,6 @@
 Unsplash.configure do |config|
-  config.application_access_key = "<YOUR KEY HERE>"
-  config.application_secret = "<YOUR KEY HERE>"
+  config.application_access_key = ENV['UNSPLASH_ACCESS_KEY']
+  config.application_secret = ENV['UNSPLASH_SECRET']
   config.application_redirect_uri = "https://your-application.com/oauth/callback"
   config.utm_source = "support-your-local-businesses"
 end
