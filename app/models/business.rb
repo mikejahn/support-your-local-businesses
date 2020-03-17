423ead928092ab58e81c83b2e6f39fc99acce0a0
Mike <themikejahn@gmail.com>
2020-03-17 11:04:52 -0500

    Remove duplicate validations in business model

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,7 +1,4 @@
 class Business < ApplicationRecord
-  validates :name, presence: true
-  validates :website, presence: true
-
   belongs_to :city
 
   validates :name, :website, :address, :gift_card_link, presence: true
